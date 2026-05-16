769e21316f1a52f9a9445cdca0e896ac8ae13626
dianaquispeayala369-hub <dianaquispeayala369@gmail.com>
2026-05-16 15:16:15 -0500

    Fix deb

--- main.py
+++ main.py
@@ -5,6 +5,6 @@ app = Flask(__name__)
 
 base_de_datos = ["jorge","pepe","juanito"] #Modelos
 
-@app.route("/index") # controlador
+@app.route("/") # controlador
 def hello():
     return render_template(template_name_or_list="index.html", datos= base_de_datos) #visualizacion o view
