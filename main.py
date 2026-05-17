158619341b8bb58ca2d343f1d5f3f2881de0fa70
dianaquispeayala369-hub <dianaquispeayala369@gmail.com>
2026-05-17 17:28:10 -0500

    feat: base de datos y esquema

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
-from flask import Flask, render_template, request
+from flask import Flask, redirect, render_template, request, url_for, abort
 
 app = Flask(__name__)
 
-@app.route("/") # controlador
+
+@app.route("/")  # controlador
 def root():
     return render_template("base.html")
 
-@app.route("/index") # controlador
-def index():
-    base_de_datos = ["jorge","pepe","juanito"] #Modelos
-    return render_template("index.html", datos= base_de_datos) #visualizacion o view
 
-@app.route("/home") # controlador
+@app.route("/index")  # controlador
+def index():
+    base_de_datos = ["jorge", "pepe", "juanito"]  # Modelos
+    return render_template("index.html", datos=base_de_datos)  # visualizacion o view
+
+
+@app.route("/home")  # controlador
 def home():
     return render_template("home.html")
